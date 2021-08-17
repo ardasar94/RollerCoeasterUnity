38d32a99dfb555d25cb27204e5aaea96bfbb8529
ardasarihan <arda.sarihan@bilgeadam.com>
2021-08-17 13:31:40 +0300

    RollerCoasterGameInitial

---
+++ New Unity Project/Assets/GameController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] GameObject StartScreen;
+    [SerializeField] Text moneyText;
+    [SerializeField] Text currentMoneyText;
+    [SerializeField] Text tourText;
+    [SerializeField] GameObject EndScreen;
+
+    CarControls carControls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        carControls = FindObjectOfType<CarControls>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = carControls.totalMoney.ToString();
+        currentMoneyText.text = "Current Money: " + carControls.totalMoney.ToString();
+        tourText.text = "Current Tour: " + carControls.CurrentTour.ToString();
+    }
+
+    public void GetInput1(string guess)
+    {
+        string basics = guess;
+        string basicsCheck = basics.Replace(" ", "");
+        int x;
+        List<string> basicsList = basics.Split(' ').ToList();
+        if (!int.TryParse(basicsCheck, out x) || basicsList.Count != 3)
+        {
+            Debug.Log("You should enter the nums with given rules");
+            return;
+        }
+
+        carControls.l = int.Parse(basicsList[0]);
+        carControls.c = int.Parse(basicsList[1]);
+        carControls.n = int.Parse(basicsList[2]);
+
+        if (carControls.l < 1 || carControls.l > Math.Pow(10, 7) || carControls.c < 1 || carControls.c > Math.Pow(10, 7) || carControls.n < 1 || carControls.n > 1000)
+        {
+            Console.WriteLine("You should enter the nums with given rules");
+            return;
+        }
+    }
+    public void GetInput2(string guess)
+    {
+        carControls.groups = new List<int>();
+        string groups = guess;
+        string groupsCheck = groups.Replace(" ", "");
+        int x;
+        List<string> groupsList = groups.Split(' ').ToList();
+        if (!int.TryParse(groupsCheck, out x) || groupsList.Count != carControls.n)
+        {
+            Debug.Log("You should enter the nums with given rules");
+            return;
+        }
+
+        for (int i = 0; i < carControls.n; i++)
+        {
+            carControls.groups.Add(int.Parse(groupsList[i]));
+        }
+    }
+
+    public void StartGame()
+    {
+        StartScreen.SetActive(false);
+        carControls.shouldStart = true;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); ;
+    }
+
+    public void ShowEndScreen()
+    {
+        Invoke("EndScreenSetActiveTrue", 1.5f);
+    }
+
+    public void EndScreenSetActiveTrue()
+    {
+        EndScreen.SetActive(true);
+    }
+
+}
---
+++ New Unity Project/Assets/Scripts/CarControls.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CarControls : MonoBehaviour
+{
+    [SerializeField] GameObject Person;
+    [SerializeField] Animator animator;
+
+    public int l = 3, c = 3, n = 4;
+    public List<int> groups = new List<int>() { 3, 1, 1, 2 };
+    public int totalMoney = 0;
+    public bool shouldStart = false;
+    public int CurrentTour = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shouldStart)
+        {
+            StartFun();
+            shouldStart = false;
+        }
+        if (c == CurrentTour)
+        {
+            StopAllCoroutines();
+            animator.SetBool("isRun", false);
+            FindObjectOfType<GameController>().ShowEndScreen();
+        }
+    }
+
+    private void StartFun()
+    {
+        InstantiatePeopleAtStart();
+        StartCoroutine("StartCar");
+    }
+
+    private void InstantiatePeopleAtStart()
+    {
+        int totalPeople = groups.Sum();
+        GameObject line = GameObject.Find("Line");
+
+        for (int i = 0; i < totalPeople; i++)
+        {
+            if (i == 0)
+            {
+                GameObject newPerson = Instantiate(Person, line.transform.position, Quaternion.identity);
+                newPerson.transform.parent = line.transform;
+            }
+            else
+            {
+                GameObject newPerson = Instantiate(Person, line.transform.position + (line.transform.GetChild(line.transform.childCount - 1)).transform.localPosition + new Vector3(0.6f, 0, 0), Quaternion.identity);
+                newPerson.transform.parent = line.transform;
+            }
+        }
+    }
+
+    IEnumerator StartCar()
+    {
+        while (CurrentTour < c)
+        {
+            animator.SetBool("isRun", false);
+            int oneRideSize = CheckOneRide(groups, l);
+            SwipeTheLine(oneRideSize);
+            totalMoney += oneRideSize;
+            CurrentTour++;
+            Debug.Log(CurrentTour);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    int x = 0;
+    private void SwipeTheLine(int oneRideSize)
+    {
+        x = oneRideSize;
+        GameObject line = GameObject.Find("Line");
+        for (int i = 0; i < oneRideSize; i++)
+        {
+            Destroy(line.transform.GetChild(i).gameObject);
+        }
+        StartCoroutine(FallowPath(oneRideSize));
+        animator.SetBool("isRun", true);
+
+        Invoke("InstantiatePeopleAtRunTime", 1);
+    }
+
+    IEnumerator FallowPath(int oneRideSize)
+    {
+        GameObject line = GameObject.Find("Line");
+        Vector3 startPosition = line.transform.position;
+        Vector3 endPosition = new Vector3(line.transform.transform.position.x - 0.6f * oneRideSize, line.transform.transform.position.y, line.transform.transform.position.z); ;
+        float travelPercent = 0f;
+
+        transform.LookAt(endPosition);
+
+        while (travelPercent < 1f)
+        {
+            travelPercent += Time.deltaTime * 1f;
+            line.transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private void InstantiatePeopleAtRunTime()
+    {
+        GameObject line = GameObject.Find("Line");
+
+        for (int i = 0; i < x; i++)
+        {
+            if (line.transform.childCount == 0)
+            {
+                GameObject newPerson = Instantiate(Person, line.transform.position, Quaternion.identity);
+                newPerson.transform.parent = line.transform;
+            }
+            else
+            {
+                GameObject newPerson = Instantiate(Person, line.transform.position + (line.transform.GetChild(line.transform.childCount - 1)).transform.localPosition + new Vector3(0.6f, 0, 0), Quaternion.identity);
+                newPerson.transform.parent = line.transform;
+            }
+        }
+    }
+
+    private int CheckOneRide(List<int> groups, int L)
+    {
+        int multiply = 0;
+        int oneGroupSize = 0;
+        foreach (var item in groups)
+        {
+            if (oneGroupSize + item > L)
+            {
+                for (int i = 0; i < multiply; i++)
+                {
+                    int x = groups[0];
+                    for (int j = 1; j < groups.Count; j++)
+                    {
+                        groups[j - 1] = groups[j];
+                    }
+                    groups[groups.Count - 1] = x;
+                }
+
+                return oneGroupSize;
+            }
+            oneGroupSize += item;
+            multiply++;
+        }
+        return oneGroupSize;
+    }
+}
